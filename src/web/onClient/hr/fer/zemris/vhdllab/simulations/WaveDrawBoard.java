b41a8925aeaa7f253b08e5cc398d49381076c72c
Borislav Ožegović <borislav.ozegovic@no-email-known.hr>
2006-09-06 11:37:40 +0000

    Dodana mogucnost promjene poretka signala
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@10 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- src/web/onClient/hr/fer/zemris/vhdllab/simulations/WaveDrawBoard.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/simulations/WaveDrawBoard.java
@@ -3,243 +3,9 @@ package hr.fer.zemris.vhdllab.simulations;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Map;
-
 import javax.swing.JPanel;
 
-/**
- * Crtanje vallnog oblika
- *
- * @author Boris Ozegovic
- */
-class WaveForm
-{
-    /* svi oblici koji su jednaki nuli pripadaju grupi nula */
-    private final byte ZERO_SHAPES = 1;
 
-    /* svi oblici u jedinici */
-	private final byte ONE_SHAPES = 2;
-
-    /* svi oblici koji predstavljaju vektor ili su neinicijalizirani */
-	private final byte HEXAGON_SHAPES = 3;
-    private final int WAVE_START_POINT_IN_PIXELS = 0;
-    private final int SCREEN_SIZE_IN_PIXELS = 2000;
-
-    /* vrijednosti valnih oblika */
-	private java.util.List <String> values;
-	private int[] durationsInPixels; 
-    private int screenStartPointInPixels;
-    private int screenEndPointInPixels;
-    private int waveEndPointInPixels;
-
-    /* svi valni oblici */
-    private Shape[] shapes = new Shape[] {new Shape1(), new Shape2(), new Shape3(),
-                                          new Shape4(), new Shape5(), new Shape6(), 
-                                          new Shape7(), new Shape8(), new Shape9() };
-
-
-    /**
-     * Constructor
-     *
-     * @param values lista vrijednosti za svaki
-     * @param durationsInPixels vrijeme trjanja u pikselima
-     */
-	public WaveForm (java.util.List <String> values, int[] durationsInPixels)
-	{
-		this.values = values;
-        this.durationsInPixels = durationsInPixels;
-	}
-	
-	
-    /**
-     * Metoda koji odlucuje koji dio valnih oblika ce se iscrtati u ovisnosti u
-     * screenEndPointInPixels
-     * 
-     * @param g Graphics
-     * @param yAxis na koji dio y-osi se crta
-     * @param offsetXAxis ovisno o offsetu crta dio ekrana
-     * @param durationsInPixels trajanje dobivena iz skale
-     * @param waveEndPointInPixels dobivenu iz skale
-     */
-	public void drawWave (Graphics g, int yAxis, int offsetXAxis, int[] durationsInPixels,
-                          int waveEndPointInPixels) 
-	{
-        this.durationsInPixels = durationsInPixels;
-
-        /* 
-         * postoji sadasnja i sljedeca grupa, jer postoji vise vrsta oblika,
-         * npr. jedinica koja slijedi iza nule drugacije je od jednice koja
-         * slijedi iza vektora
-         */
-		byte previousGroup = 0;
-		byte presentGroup = 0;
-        screenStartPointInPixels = offsetXAxis;
-        screenEndPointInPixels = screenStartPointInPixels + SCREEN_SIZE_IN_PIXELS;
-        int x1 = WAVE_START_POINT_IN_PIXELS;
-		int y1 = yAxis;
-		int x2;
-  
-        /* 
-         * samo ako nije jednak nuli screenStartPointInPixels je umanjen za
-         * jedan zato da ne iscrta na ekranu pocetak signala, vec treba ostaviti
-         * dojam da signal traje od prije 
-         */
-        if (screenStartPointInPixels != WAVE_START_POINT_IN_PIXELS)
-        {
-            screenStartPointInPixels -= 1 ;
-        }
-
-		int i = 0;
-
-        /* za svaku pojedinu vrijednost signala */
-		for (String string : values)
-        {
-			x2 = x1 + durationsInPixels[i]; 
-            if (durationsInPixels[i] == 0)
-            {
-                i++;
-                continue;
-            }
-
-			if (string.equals("0"))
-            {
-				presentGroup = ZERO_SHAPES;
-			}
-			else if (string.equals("1"))
-            {
-				presentGroup = ONE_SHAPES;
-			}
-			else 
-            {
-				presentGroup = HEXAGON_SHAPES;
-			}
-            
-            /* 
-             * ako je x2 manji od pocetka na kojem treba iscrtati jednostavno
-             * ignorira te signale
-             */
-            if (x2 > screenStartPointInPixels)
-            {
-                /* 
-                 * ako neki signal ide preko ekrana pocinje njegovo iscrtavanje,
-                 * medutim, treba ograniciti jer signalmoze trajati po tisucu
-                 * piksela, a  moze biti i i manji od screenEndPointInPixels
-                 */
-                if (x2 < screenEndPointInPixels)
-                {
-                    draw(g, string, previousGroup, presentGroup, 
-                            screenStartPointInPixels - offsetXAxis, y1, 
-                            x2 - offsetXAxis);
-                }
-                else
-                {
-                    /* ako je screenEndPointInPixels jednak tocno kraju skale */
-                    if (screenEndPointInPixels == waveEndPointInPixels)
-                    {
-                        draw(g, string, previousGroup, presentGroup, 
-                                screenStartPointInPixels - offsetXAxis, 
-                                y1, screenEndPointInPixels - offsetXAxis);
-                    }
-
-                    /* 
-                     * inace crta s jednim prosirenim pikselom koji daje dojam
-                     * da signal ne zavrsava, vec se nastavlja
-                     */
-                    else
-                    {
-                        draw(g, string, previousGroup, presentGroup, 
-                                screenStartPointInPixels - offsetXAxis, 
-                                y1, screenEndPointInPixels + 1 - offsetXAxis);
-                        break;
-                    }
-                }
-                screenStartPointInPixels = x2;
-            }
-			previousGroup = presentGroup;
-			x1 = x2;
-			i++;
-		}
-	}
-
-
-    /**
-     * Metoda za samo iscrtavanje
-     *
-     * @param g Graphics
-     * @param sring vrijednost signala
-     * @param previousGroup prijasnja grupa
-     * @param presentGroup sadasnja grupa
-     * @param x1 vrijednost od koje pocinje crtanje
-     * @param y1 vrijednost po y-osi
-     * @param x2 trajanje ove vrijednosti signala u pikselima
-     */
-    public void draw (Graphics g, String string, int previousGroup, 
-                      int presentGroup, int x1, int y1, int x2)
-    {
-        switch (previousGroup)
-        {
-            case ZERO_SHAPES : 
-                switch (presentGroup)
-                {
-                    case ZERO_SHAPES : 
-                        shapes[0].draw(g, x1, y1, x2); 
-                        break;
-                    case ONE_SHAPES : 
-                        shapes[4].draw(g, x1, y1, x2); 
-                        break;
-                    case HEXAGON_SHAPES : 
-                        shapes[8].draw(g, x1, y1, x2); 
-                        shapes[8].putLabel(g, string, x1, y1, x2); 
-                        break;
-                }
-                break;
-            case ONE_SHAPES : 
-                switch (presentGroup)
-                {
-                    case ZERO_SHAPES : 
-                        shapes[1].draw(g, x1, y1, x2); 
-                        break;
-                    case ONE_SHAPES : 
-                        shapes[3].draw(g, x1, y1, x2); 
-                        break;
-                    case HEXAGON_SHAPES : 
-                        shapes[7].draw(g, x1, y1, x2); 
-                        shapes[7].putLabel(g, string, x1, y1, x2); 
-                        break;
-                }
-                break;
-            case HEXAGON_SHAPES :
-                switch (presentGroup)
-                {
-                    case ZERO_SHAPES : 
-                        shapes[2].draw(g, x1, y1, x2); 
-                        break;
-                    case ONE_SHAPES : 
-                        shapes[5].draw(g, x1, y1, x2);
-                        break;
-                    case HEXAGON_SHAPES : 
-                        shapes[6].draw(g, x1, y1, x2); 
-                        shapes[6].putLabel(g, string, x1, y1, x2); 
-                        break;
-                }
-                break;
-            default : 
-                switch (presentGroup)
-                {
-                    case ZERO_SHAPES : 
-                        shapes[0].draw(g, x1, y1, x2);
-                        break;
-                    case ONE_SHAPES : 
-                        shapes[4].draw(g, x1, y1, x2);
-                        break;
-                    case HEXAGON_SHAPES : 
-                        shapes[6].draw(g, x1, y1, x2);
-                        shapes[6].putLabel(g, string, x1, y1, x2);
-                        break;
-                }
-        }
-    }
-}
 
 
 /**
@@ -255,10 +21,11 @@ class WaveDrawBoard extends JPanel
     private final int FIRST_WAVE_YAXIS_START = 20;
 
     /* svaki spring (elasticni pretinac) vala iznosi 40 piksela */
-    private final int WAVE_SPRING_SIZE = 40;
+    private final int WAVE_SPRING_SIZE = 45;
     private final Color BACKGROUND_COLOR = new Color (201, 211, 236);
-    private Map <String, java.util.List <String>> signalValues;
-
+    
+    /* vrijednosti signala */
+    private String[][] signalValues;
     /* trajanje u pikselima svake od vrijednosti u signalValues */
     private int[] durationsInPixels;
 
@@ -273,24 +40,30 @@ class WaveDrawBoard extends JPanel
     /* panel se sastoji od polja valnih oblika */
     private WaveForm[] waveForm;
     private Scale scale;
+    
+    /* varijabla koja sadrzi informaciju je li trenutni signal oznacen misem */
+    private boolean isClicked = false;
+
+    /* sadrzi indeks trenutno oznacenog signala misem */
+    private int index = -1;
 
 
     /**
      * Constructor
      *
-     * @param signalValues vrijednosti dobivene iz VcdParsera
+     * @param results rezultati simulacije, parsirani od GhdlResults klase
      * @param scale skala
      */
-    public WaveDrawBoard (Map <String, java.util.List <String>> signalValues, Scale scale)
+    public WaveDrawBoard (GhdlResults results, Scale scale)
     {
         super();
-        this.signalValues = signalValues;
+        this.signalValues = results.getSignalValues();
 
         /* skala proracunava sve parametre i panel koristi gotove parametre */
         this.durationsInPixels = scale.getDurationInPixels();
         this.waveEndPointInPixels = scale.getScaleEndPointInPixels();
         this.scale = scale;
-        waveForm = new WaveForm[signalValues.size()];
+        waveForm = new WaveForm[signalValues.length];
         setBackground(BACKGROUND_COLOR);
         int i = 0;
 
@@ -298,9 +71,9 @@ class WaveDrawBoard extends JPanel
          * inicijalizacija pojedinih valnih oblika.  Svakom se valnom obliku
          * odmah dodaje lista vrijednosti (1, 0, U, Z...)
          */
-        for (java.util.List <String> lista : signalValues.values())
+        for (String[] polje : signalValues)
         {
-            waveForm[i++] = new WaveForm(lista, durationsInPixels);
+            waveForm[i++] = new WaveForm(polje, durationsInPixels);
         }
     }
 
@@ -311,7 +84,7 @@ class WaveDrawBoard extends JPanel
 
     public Dimension getPreferredSize() 
     { 
-        return new Dimension(waveEndPointInPixels, signalValues.size() * WAVE_SPRING_SIZE);
+        return new Dimension(waveEndPointInPixels, signalValues.length * WAVE_SPRING_SIZE);
     } 
 
 	
@@ -332,6 +105,7 @@ class WaveDrawBoard extends JPanel
         return offsetXAxis;
     }
     
+
     /**
      * Setter
      */
@@ -358,6 +132,55 @@ class WaveDrawBoard extends JPanel
         this.waveEndPointInPixels = waveEndPointInPixels;
     }
 
+
+    /**
+     * Metoda koja postavlja novi set vrijednosti signala 
+     */
+    public void setSignalValues (String[][] signalValues)
+    {
+        this.signalValues = signalValues;
+    }
+
+
+    /**
+     * Postavlja informaciju treba li oznaciti signal
+     *
+     * @param isClicked true or false
+     */
+    public void setIsClicked (boolean isClicked)
+    {
+        this.isClicked = isClicked;
+    }
+
+
+    /**
+     * Postavlja trenutni indeks oznacenog signala 
+     *
+     * @param index indeks oznacenog signala
+     */
+    public void setIndex (int index)
+    {
+        this.index = index;
+    }
+
+
+    /**
+     * Vraca informaciju je li signal oznacen
+     */
+    public boolean getIsClicked ()
+    {
+        return isClicked;
+    }
+
+
+    /**
+     * Vraca indeks trenutno oznacenog signala
+     */
+    public int getIndex ()
+    {
+        return index;
+    }
+
 	
     
     /**
@@ -368,9 +191,41 @@ class WaveDrawBoard extends JPanel
 		super.paintComponent(g);
         waveEndPointInPixels = scale.getScaleEndPointInPixels();
 		yAxis = FIRST_WAVE_YAXIS_START - offsetYAxis;
+
+        /* crtanje isprekidanih crta */
+        //g.setColor(new Color(255, 241, 255));
+        g.setColor(new Color(163, 179, 225));
+        int x = 100 - offsetXAxis % 100;
+        int y = 0;
+        while (x < this.getWidth() + 100)
+        {
+            y = 0;
+            while (y < this.getHeight() + 100)
+            {
+                g.drawLine(x, y, x, y + 8);
+                y += 12;
+            }
+            x += 100;
+        }
+
+         /* ako treba oznaciti neki od signala */
+        if (isClicked)
+        {
+            g.setColor(new Color(254, 217, 182));
+            g.fillRect(0, (index * WAVE_SPRING_SIZE) + 15 - offsetYAxis,
+                    getPreferredSize().width, WAVE_SPRING_SIZE / 2 + 5);
+        }
+        g.setColor(new Color(51, 51, 51));
+
+        /* crtanje valova */
+        g.setColor(new Color(51, 51, 51));
+        for (int i = 0; i < waveForm.length; i++)
+        {
+            waveForm[i].setSignalValues(signalValues[i]);
+        }
 		for (WaveForm wave : waveForm)
 		{
-			wave.drawWave(g, yAxis, offsetXAxis, durationsInPixels, waveEndPointInPixels);
+			wave.drawWave(g, this.getWidth(), yAxis, offsetXAxis, durationsInPixels, waveEndPointInPixels);
 			yAxis += WAVE_SPRING_SIZE;
 		}
 	}
