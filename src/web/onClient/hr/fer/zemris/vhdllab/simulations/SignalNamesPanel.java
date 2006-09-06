388d2ce82255552b70f210b6926bd813b6b86678
Borislav Ožegović <borislav.ozegovic@no-email-known.hr>
2006-09-06 11:39:18 +0000

    Dodana mogucnost promjene poretka signala
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@11 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- src/web/onClient/hr/fer/zemris/vhdllab/simulations/SignalNamesPanel.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/simulations/SignalNamesPanel.java
@@ -3,9 +3,9 @@ package hr.fer.zemris.vhdllab.simulations;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Map;
-
 import javax.swing.JPanel;
+
+
 
 /**
  * Panel koji sadrzi imena signala
@@ -20,13 +20,13 @@ class SignalNamesPanel extends JPanel
     private final int YAXIS_START_POINT = 30;
 
     /* svako se ime nalazi u springu (elasticnom pretincu) koji je visine 40 piksela */
-	private final int SIGNAL_NAME_SPRING_HEIGHT = 40;
+	private final int SIGNAL_NAME_SPRING_HEIGHT = 45;
 
     /* maksimalna duljina koju panel moze poprimiti iznosi 150 piksela */
 	private final int PANEL_MAX_WIDTH = 150;
 
-    /* mapa koja je dobivena iz VcdParsera i ciji kljucevi jesu imena signala */
-    private Map <String, java.util.List <String>> signalValues;
+    /* polje Stringova koje sadrzi sva imena signala */
+    private String[] signalNames;
 
     /* polozaj trenutnog springa */
     private int yAxis;
@@ -34,20 +34,24 @@ class SignalNamesPanel extends JPanel
     private int offsetXAxis;
     private int maximumSignalNameLength;
 
+    /* varijabla koja sadrzi informaciju je li trenutni signal oznacen misem */
+    private boolean isClicked = false;
+
+    /* sadrzi indeks trenutno oznacenog signala misem */
+    private int index = -1;
+
 
     /**
      * Constructor
      *
-     * @param signalValues kljucevi mape jesu imena signala, dobiveno iz parsera 
-     * @param maximumSignalNameLength ime najvece duljine
+     * @param results rezultati dobiveni HTTP-om parsirani od GhdlResults klase 
      */
-    public SignalNamesPanel (Map <String, java.util.List <String>> signalValues, 
-			int maximumSignalNameLength)
+    public SignalNamesPanel (GhdlResults results)
     {
         super();
         setBackground(BACKGROUND_COLOR);
-        this.signalValues = signalValues;
-        this.maximumSignalNameLength = maximumSignalNameLength;
+        this.signalNames = results.getSignalNames();
+        this.maximumSignalNameLength = results.getMaximumSignalNameLength();
     }
     
       
@@ -57,7 +61,7 @@ class SignalNamesPanel extends JPanel
     public Dimension getPreferredSize() 
     { 
         return new Dimension(maximumSignalNameLength * 6 + 4, 
-				signalValues.size() * SIGNAL_NAME_SPRING_HEIGHT); 
+				signalNames.length * SIGNAL_NAME_SPRING_HEIGHT); 
     } 
 
 
@@ -70,11 +74,11 @@ class SignalNamesPanel extends JPanel
         if (maximumSignalNameLength * 6 < PANEL_MAX_WIDTH)
         {
             return new Dimension(maximumSignalNameLength * 6, 
-					signalValues.size() * SIGNAL_NAME_SPRING_HEIGHT); 
+					signalNames.length * SIGNAL_NAME_SPRING_HEIGHT); 
         }
         else
         {
-            return new Dimension(PANEL_MAX_WIDTH, signalValues.size() * SIGNAL_NAME_SPRING_HEIGHT);
+            return new Dimension(PANEL_MAX_WIDTH, signalNames.length * SIGNAL_NAME_SPRING_HEIGHT);
         }
     }
  
@@ -89,11 +93,69 @@ class SignalNamesPanel extends JPanel
 
 
     /**
+     * Vraca trenutni vertikalni offset
+     */
+    public int getVerticalOffset ()
+    {
+        return offsetYAxis;
+    }
+
+
+    /**
      * Setter koji postavlja horizontalni offset
      */
     public void setHorizontalOffset (int offset)
     {
         this.offsetXAxis = offset;
+    }
+
+
+    /**
+     * Metoda postavlja novi set imena signala
+     */
+    public void setSignalNames (String[] signalNames)
+    {
+        this.signalNames = signalNames;
+    }
+
+
+    /**
+     * Postavlja informaciju treba li oznaciti signal
+     *
+     * @param isClicked true or false
+     */
+    public void setIsClicked (boolean isClicked)
+    {
+        this.isClicked = isClicked;
+    }
+
+
+    /**
+     * Postavlja trenutni indeks oznacenog signala 
+     *
+     * @param index indeks oznacenog signala
+     */
+    public void setIndex (int index)
+    {
+        this.index = index;
+    }
+
+
+    /**
+     * Vraca informaciju je li signal oznacen
+     */
+    public boolean getIsClicked ()
+    {
+        return isClicked;
+    }
+
+
+    /**
+     * Vraca indeks trenutno oznacenog signala
+     */
+    public int getIndex ()
+    {
+        return index;
     }
 
     
@@ -103,11 +165,21 @@ class SignalNamesPanel extends JPanel
     public void paintComponent (Graphics g)
 	{
 		super.paintComponent(g);
+
+        /* ako treba oznaciti neki od signala */
+        if (isClicked)
+        {
+            g.setColor(new Color(254, 217, 182));
+            g.fillRect(0, (index * SIGNAL_NAME_SPRING_HEIGHT) + 15 -offsetYAxis, 
+                    getMaximumSize().width, SIGNAL_NAME_SPRING_HEIGHT / 2 + 5);
+        }
+        g.setColor(new Color(51, 51, 51));
 		yAxis = YAXIS_START_POINT - offsetYAxis;
-		for (String string : signalValues.keySet())
+		for (String string : signalNames)
 		{
 			g.drawString(string, 5 - offsetXAxis, yAxis);
 			yAxis += SIGNAL_NAME_SPRING_HEIGHT;
 		}
 	}
 }
+
