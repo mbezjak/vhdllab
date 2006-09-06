021c325d60e29efa52508eefcd3b04b56b63ba75
Miro Bezjak <bezjak.miro@gmail.com>
2006-09-06 15:09:10 +0000

    renamed src/tests/web/OnServer to         src/tests/web/onServer
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@20 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/tests/web/onServer/hr/fer/zemris/vhdllab/AllOnServerJUnitTests.java
@@ -0,0 +1,17 @@
+package hr.fer.zemris.vhdllab;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * This TestCase tests each Test in src/tests/web/onServer.
+ */
+public class AllOnServerJUnitTests extends TestCase {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		
+		return suite;
+	}
+}
