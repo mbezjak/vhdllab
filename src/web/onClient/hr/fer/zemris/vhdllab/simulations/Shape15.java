c02bd989aa93d6b3e4ca1b29e089a08b5fb90cef
Borislav Ožegović <borislav.ozegovic@no-email-known.hr>
2006-09-22 08:54:03 +0000

    Dodan novi oblik.
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@32 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/simulations/Shape15.java
@@ -0,0 +1,34 @@
+package hr.fer.zemris.vhdllab.simulations;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+/**
+ * Predstavlja visoku impedanciju s poluvertikalnom crtom prema dolje. Dolazi
+ * iza nule
+ *
+ * @author Boris Ozegovic
+ */
+class Shape15 implements Shape
+{
+    public void draw (Graphics g, int x1, int y1, int x2)
+    {
+        g.setColor(new Color(119, 119, 192));
+        g.drawLine(x1, y1 + 8, x2, y1 + 8);
+        g.drawLine(x1, y1 + 9, x2, y1 + 9);
+        g.drawLine(x1, y1 + 10, x2, y1 + 10);
+        g.drawLine(x1, y1 + 11, x2, y1 + 11);
+        g.drawLine(x1, y1 + 10, x1, y1 + 20);    
+        g.setColor(new Color(51, 51, 51));
+    }
+
+
+    public void putLabel(Graphics g, String s, int x1, int y1, int x2)
+	{
+		if (x2 - x1 >= 10)
+        {
+            g.drawString(s, x1, y1 + 5);
+        }
+	}
+}
