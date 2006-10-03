3b785fdda9c79c302b60f6acccf12cd9f10cdd05
Miro Bezjak <bezjak.miro@gmail.com>
2006-10-03 21:54:53 +0000

    added two models to describe general options and owner's local options
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@61 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/dao/hr/fer/zemris/vhdllab/model/LocalOptions.java
@@ -0,0 +1,49 @@
+package hr.fer.zemris.vhdllab.model;
+
+/**
+ * @hibernate.class
+ *  table = "LOCALOPTIONS"
+ */
+public class LocalOptions {
+	
+	private Long id;
+	private Long ownerID;
+	private String content;
+	
+	public LocalOptions() {}
+	
+	/**
+	 * @hibernate.id
+	 *  generator-class = "native"
+	 */
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @hibernate.property
+	 *  not-null = "true"
+	 *  unique = "true"
+	 */
+	public Long getOwnerID() {
+		return ownerID;
+	}
+	public void setOwnerID(Long ownerID) {
+		this.ownerID = ownerID;
+	}
+	
+	/**
+	 * @hibernate.property
+	 *  not-null = "true"
+	 *  length = "1024"
+	 */
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
