a4a60216c88c8a349191899be51169b6600036fc
Miro Bezjak <bezjak.miro@gmail.com>
2006-10-06 17:11:44 +0000

    added MainApplet
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@65 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/main/MainApplet.java
@@ -0,0 +1,82 @@
+package hr.fer.zemris.vhdllab.main;
+
+import hr.fer.zemris.ajax.shared.AjaxMediator;
+import hr.fer.zemris.ajax.shared.AjaxOpListener;
+import hr.fer.zemris.ajax.shared.DefaultAjaxMediator;
+import hr.fer.zemris.ajax.shared.XMLUtil;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.Properties;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class MainApplet extends JApplet implements AjaxOpListener {
+	
+	/**
+	 * Serial version ID.
+	 */
+	private static final long serialVersionUID = 4037604752375048576L;
+	
+	private AjaxMediator ajax;
+	
+	@Override
+	public void init() {
+		super.init();
+		ajax = new DefaultAjaxMediator(this);
+		ajax.registerResultListener(this);
+		initGUI();
+	}
+	
+	@Override
+	public void start() {
+		super.start();
+	}
+	
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+	
+	@Override
+	public void destroy() {
+		ajax.initiateAbort();
+		ajax = null;
+		super.destroy();
+	}
+	
+	private void initGUI() {
+		JPanel projectExplorerPanel = new JPanel(new BorderLayout());
+		JLabel l1 = new JLabel();
+		projectExplorerPanel.add(l1, BorderLayout.CENTER);
+		projectExplorerPanel.setPreferredSize(new Dimension(this.getWidth()/5,0));
+
+		JPanel writerPanel = new JPanel(new BorderLayout());
+		JTextArea ta = new JTextArea();
+		writerPanel.add(ta, BorderLayout.CENTER);
+		JPanel statusPanel = new JPanel(new BorderLayout());
+		statusPanel.add(l1, BorderLayout.CENTER);
+		statusPanel.setPreferredSize(new Dimension(0,this.getHeight()/5));
+		
+		this.getContentPane().add(projectExplorerPanel, BorderLayout.WEST);
+		this.getContentPane().add(writerPanel, BorderLayout.CENTER);
+		this.getContentPane().add(statusPanel, BorderLayout.SOUTH);
+
+		
+		// TODO Auto-generated method stub
+	}
+	
+	public void resultReceived(String result, int code) {
+		if(code!=200) return;
+		Properties p = XMLUtil.deserializeProperties(result);
+	}
+	
+	public void ajaxCallResultReceived(String result, int code) {
+		ajax.fireResultReceived(result,code);
+	}
+	
+}
