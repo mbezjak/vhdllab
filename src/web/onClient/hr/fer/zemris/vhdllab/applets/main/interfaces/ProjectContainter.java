a65ffe8c4868a9d8e79c7f577b680aaa9e8b9bfa
Miro Bezjak <bezjak.miro@gmail.com>
2006-11-01 13:23:36 +0000

    added two methods for getting options and ResourceBundle
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@100 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- src/web/onClient/hr/fer/zemris/vhdllab/applets/main/interfaces/ProjectContainter.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/main/interfaces/ProjectContainter.java
@@ -1,10 +1,13 @@
 package hr.fer.zemris.vhdllab.applets.main.interfaces;
 
 import java.util.List;
+import java.util.ResourceBundle;
 
 import hr.fer.zemris.vhdllab.vhdl.model.CircuitInterface;
 
 public interface ProjectContainter {
 	List<String> getAllCircuits();
 	CircuitInterface getCircuitInterfaceFor(String name);
+	String getOptions();
+	ResourceBundle getResourceBundle();
 }
