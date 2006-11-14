e21e43f32b601c0d63694a7394505494bda0791a
Aleksandar Prokopec <aleksandar.prokopec@no-email-known.hr>
2006-11-14 21:10:15 +0000

    Ovo je objekt koji vraca komponenta pozivom getPropertyList. U ovom trenutku ne sadrzi jos nista, jer nije jos sasvim jasno kako ga implementirati.
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@112 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema/ComponentPropertyList.java
@@ -0,0 +1,11 @@
+package hr.fer.zemris.vhdllab.applets.schema;
+
+/**
+ * Objekt koji vraca svaka komponenta
+ * a koji sadrzi sva svojstva dane
+ * komponente.
+ * @author Axel
+ *
+ */
+public class ComponentPropertyList {
+}
