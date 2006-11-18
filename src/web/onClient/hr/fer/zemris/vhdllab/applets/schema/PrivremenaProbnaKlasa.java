3f51b97faef45948bddcaf0ddb54083d9a958992
Aleksandar Prokopec <aleksandar.prokopec@no-email-known.hr>
2006-11-18 00:14:08 +0000

    Kao sto samo ime kaze.
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@120 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema/PrivremenaProbnaKlasa.java
@@ -0,0 +1,35 @@
+package hr.fer.zemris.vhdllab.applets.schema;
+
+import hr.fer.zemris.vhdllab.applets.schema.components.PrivremeniProbniSklop;
+
+import javax.swing.JFrame;
+
+public class PrivremenaProbnaKlasa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setSize(250, 150);
+		frame.setVisible(true);
+				
+		PrivremeniProbniSklop probni = new PrivremeniProbniSklop();
+		
+		PropertyPanel panel = new PropertyPanel(probni.getPropertyList());
+		frame.add(panel);
+		frame.validate();
+		
+		while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				break;
+			}
+			System.out.println(probni.getComponentInstanceName());
+		}
+	}
+
+}
