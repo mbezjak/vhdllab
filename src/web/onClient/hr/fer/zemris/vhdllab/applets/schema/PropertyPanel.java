5d49c8f5c0c3087e28f65d594b01a7dd4983d117
Aleksandar Prokopec <aleksandar.prokopec@no-email-known.hr>
2006-11-18 00:07:42 +0000

    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@119 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema/PropertyPanel.java
@@ -0,0 +1,27 @@
+package hr.fer.zemris.vhdllab.applets.schema;
+
+import hr.fer.zemris.vhdllab.applets.schema.components.ComponentPropertyList;
+import hr.fer.zemris.vhdllab.applets.schema.components.properties.AbstractComponentProperty;
+
+import java.awt.ComponentOrientation;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PropertyPanel extends JPanel {
+	public PropertyPanel(ComponentPropertyList cplist) {
+		if (cplist != null) {
+			int i = cplist.size();
+			if (i <= 0) i = 1;
+			GridLayout grlay = new GridLayout(i, 2);
+			this.setLayout(grlay);
+			JLabel lab = null;
+			for (AbstractComponentProperty cprop : cplist) {
+				lab = new JLabel(cprop.getPropertyName());
+				this.add(lab);
+				this.add(cprop.getPropField().getComponent());
+			}
+		}
+	}
+}
