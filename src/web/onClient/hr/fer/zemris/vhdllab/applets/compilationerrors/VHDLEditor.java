580ebfc1d70fcd76ad28f9ddeb91cc95c740d934
Borislav Ožegović <borislav.ozegovic@no-email-known.hr>
2006-11-21 15:58:34 +0000

    Modificiran komentar u sucelju VHDLEditor
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@141 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- src/web/onClient/hr/fer/zemris/vhdllab/applets/compilationerrors/VHDLEditor.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/compilationerrors/VHDLEditor.java
@@ -1,8 +1,8 @@
 package hr.fer.zemris.vhdllab.applets.compilationerrors;
 
 /**
- * Sucelje koje treba implementirati VHDL editor.  Metoda se pozicionira 
- * na liniju odredenu argumentom i highlighta tu liniju
+ * Sucelje koje treba implementirati VHDL editor.  Pozivanjem metode highlightLine
+ * VHDL editor pozicionira kursor na liniju odredenu argumentom i highlighta tu liniju
  * 
  * @author Boris Ozegovic
  */
