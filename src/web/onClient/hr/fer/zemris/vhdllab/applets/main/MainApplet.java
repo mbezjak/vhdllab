542f94269f9d0285dfc38038c2655a30baa4ee8f
Miro Bezjak <bezjak.miro@gmail.com>
2006-11-29 22:27:32 +0000

    fixed .classpath duplicat import error. (digester.jar)
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@171 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- src/web/onClient/hr/fer/zemris/vhdllab/applets/main/MainApplet.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/main/MainApplet.java
@@ -24,7 +24,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
