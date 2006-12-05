f86a771dc86aafd45ddfe1fc9ed5dba42f66b0b4
Aleksandar Prokopec <aleksandar.prokopec@no-email-known.hr>
2006-12-05 20:25:10 +0000

    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@186 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema/components/ComponentFactoryException.java
@@ -0,0 +1,21 @@
+package hr.fer.zemris.vhdllab.applets.schema.components;
+
+public class ComponentFactoryException extends Exception {
+
+	public ComponentFactoryException() {
+		super();
+	}
+
+	public ComponentFactoryException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public ComponentFactoryException(String arg0) {
+		super(arg0);
+	}
+
+	public ComponentFactoryException(Throwable arg0) {
+		super(arg0);
+	}
+	
+}
