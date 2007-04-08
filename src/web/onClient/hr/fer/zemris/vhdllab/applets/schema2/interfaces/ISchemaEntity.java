a38ed98ef13ec9620d20e1d775cc0cbedbd58558
Aleksandar Prokopec <aleksandar.prokopec@no-email-known.hr>
2007-04-08 10:03:07 +0000

    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@437 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/interfaces/ISchemaEntity.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/interfaces/ISchemaEntity.java
@@ -1,5 +1,7 @@
 package hr.fer.zemris.vhdllab.applets.schema2.interfaces;
 
+import hr.fer.zemris.vhdllab.vhdl.model.CircuitInterface;
+
 
 
 /**
@@ -11,5 +13,49 @@ package hr.fer.zemris.vhdllab.applets.schema2.interfaces;
  *
  */
 public interface ISchemaEntity {
-
+	
+	
+	/**
+	 * Vraca sucelje sklopa koji se modelira
+	 * u schematicu - broj i vrstu portova.
+	 * 
+	 * @return
+	 */
+	CircuitInterface getCircuitInterface();
+	
+	/**
+	 * Postavlja sucelje sklopa.
+	 * 
+	 * @param circuitInterface
+	 */
+	void setCircuitInterface(CircuitInterface circuitInterface);
+	
+	
+	/**
+	 * Vraca mapu parametara komponente
+	 * koja se modelira u schematicu.
+	 * Na temelju ove kolekcije biti ce kasnije
+	 * izgraden generic blok pri generiranju
+	 * strukturnog VHDLa.
+	 * 
+	 * @return
+	 */
+	IParameterCollection getParameters();
+	
+	/**
+	 * Postavlja parametre (vidi metodu
+	 * getParameters()).
+	 * 
+	 * @param parameters
+	 */
+	void setParameters(IParameterCollection parameters);
+	
 }
+
+
+
+
+
+
+
+
--- src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/interfaces/ISchemaInfo.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/interfaces/ISchemaInfo.java
@@ -13,5 +13,59 @@ package hr.fer.zemris.vhdllab.applets.schema2.interfaces;
  *
  */
 public interface ISchemaInfo extends ISerializable {
-
+	
+	/**
+	 * Vraca objekt koji sadrzi sve komponente.
+	 * 
+	 * @return
+	 */
+	ISchemaComponentCollection getComponents();
+	
+	
+	/**
+	 * Postavlja objekt koji sadrzi sve komponente.
+	 * 
+	 * @param components
+	 */
+	void setComponents(ISchemaComponentCollection components);
+	
+	
+	/**
+	 * Vraca objekt koji sadrzi sve zice (signale).
+	 * 
+	 * @return
+	 */
+	ISchemaWireCollection getWires();
+	
+	
+	/**
+	 * Postavlja objekt koji sadrzi sve zice.
+	 * 
+	 * @param wires
+	 */
+	void setWires(ISchemaWireCollection wires);
+	
+	
+	/**
+	 * Vraca objekt koji sadrzi CircuitInterface
+	 * modeliranog objekta, i jos neke dodatne
+	 * podatke, npr. parametre generic bloka.
+	 * 
+	 * @return
+	 */
+	ISchemaEntity getEntity();	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
