e8cf8acc3514f2b9a85bb6f8e6b69b4ce34b4e6f
Aleksandar Prokopec <aleksandar.prokopec@no-email-known.hr>
2007-04-30 01:45:29 +0000

    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@466 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/model/commands/InstantiateComponentCommand.java
@@ -0,0 +1,41 @@
+package hr.fer.zemris.vhdllab.applets.schema2.model.commands;
+
+import hr.fer.zemris.vhdllab.applets.schema2.exceptions.InvalidCommandOperationException;
+import hr.fer.zemris.vhdllab.applets.schema2.interfaces.ICommand;
+import hr.fer.zemris.vhdllab.applets.schema2.interfaces.ICommandResponse;
+import hr.fer.zemris.vhdllab.applets.schema2.interfaces.ISchemaInfo;
+
+
+
+
+/**
+ * Ovom komandom se na shemi instancira
+ * komponenta.
+ * 
+ * @author Axel
+ *
+ */
+public class InstantiateComponentCommand implements ICommand {
+
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isUndoable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ICommandResponse performCommand(ISchemaInfo info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ICommandResponse undoCommand(ISchemaInfo info)
+			throws InvalidCommandOperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
