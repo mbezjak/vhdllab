2cb72779e265b47d5ad232d6cf893c9996131d57
Davor Delač <davor.delac@no-email-known.hr>
2007-06-25 15:20:12 +0000

    mala promjena jjos jednom :)
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@520 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/gui/canvas/ISchemaCanvas.java
@@ -0,0 +1,20 @@
+package hr.fer.zemris.vhdllab.applets.schema2.gui.canvas;
+
+import java.beans.PropertyChangeListener;
+
+import hr.fer.zemris.vhdllab.applets.schema2.interfaces.ILocalGuiController;
+import hr.fer.zemris.vhdllab.applets.schema2.interfaces.ISchemaComponentCollection;
+import hr.fer.zemris.vhdllab.applets.schema2.interfaces.ISchemaController;
+import hr.fer.zemris.vhdllab.applets.schema2.interfaces.ISchemaWireCollection;
+
+public interface ISchemaCanvas extends PropertyChangeListener{
+
+	//###########################PUBLIC METHODS##########################
+	public void setDummyStuff(ISchemaComponentCollection components,
+			ISchemaWireCollection wires);
+
+	public void registerSchemaController(ISchemaController controller);
+
+	public void registerLocalController(ILocalGuiController cont);
+
+}
