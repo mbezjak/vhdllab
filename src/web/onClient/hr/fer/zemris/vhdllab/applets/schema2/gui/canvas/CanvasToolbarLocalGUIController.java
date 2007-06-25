e40b5600b0129cf6d2d9fbe90124d8819687bb6e
Tomislav Rajaković <tomislav.rajakovic@no-email-known.hr>
2007-06-25 14:04:24 +0000

    malo izmodificiran canvas-toolbars controller (čisto da se drže sve formalnosti i izbjegne nepotreban posao kod registriranih slušača (listeners:) )
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@517 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/gui/canvas/CanvasToolbarLocalGUIController.java
+++ src/web/onClient/hr/fer/zemris/vhdllab/applets/schema2/gui/canvas/CanvasToolbarLocalGUIController.java
@@ -8,81 +8,106 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 /**
- * Razred CanvasToolbarLocalGUIController služi kao lokalni controller za SchemaCanvas i SchemaToolbar
- * u kontekstu lokalnog MVC-a za GUI scheme.
+ * Razred CanvasToolbarLocalGUIController služi kao lokalni controller za
+ * SchemaCanvas i SchemaToolbar u kontekstu lokalnog MVC-a za GUI scheme.
  * 
  * @author ddelac
- *
+ * 
  */
 public class CanvasToolbarLocalGUIController implements ILocalGuiController {
 
-	private final String propertyName = "AnyChange";
+	/**
+	 * Bilo koja promjena
+	 */
+	public static final String PROPERTY_CHANGE_COMPONENT_TO_ADD = "ChangeComponentToAdd";
+	/**
+	 * Promjena oznacene komponente (ComponentPropertyToolbar)
+	 */
+	public static final String PROPERTY_CHANGE_SELECTION = "ChangeSelection";
 
 	private PropertyChangeSupport support = null;
-	
+
 	private ECanvasState state = ECanvasState.ADD_COMPONENT_STATE;
 	private Caseless componentToAdd = null;
 	private Caseless selectedComponent = null;
-	
+
 	public CanvasToolbarLocalGUIController() {
 		support = new PropertyChangeSupport(this);
 	}
-	
-	/* (non-Javadoc)
-	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#addListener(java.beans.PropertyChangeListener)
-	 */
-	public void addListener(PropertyChangeListener listener){
-		support.addPropertyChangeListener(propertyName, listener);
+
+	public void addListener(PropertyChangeListener listener) {
+		support.addPropertyChangeListener(listener);
 	}
 
-	
-	
-	
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#addListener(java.beans.PropertyChangeListener)
+	 */
+	public void addListener(String properyName, PropertyChangeListener listener) {
+		support.addPropertyChangeListener(properyName, listener);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#getComponentToAdd()
 	 */
 	public Caseless getComponentToAdd() {
 		return componentToAdd;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#setComponentToAdd(hr.fer.zemris.vhdllab.applets.schema2.misc.Caseless)
 	 */
 	public void setComponentToAdd(Caseless componentToAdd) {
 		Caseless temp = this.componentToAdd;
 		this.componentToAdd = componentToAdd;
-		support.firePropertyChange(propertyName, temp, componentToAdd);
+		support.firePropertyChange(PROPERTY_CHANGE_COMPONENT_TO_ADD, temp,
+				componentToAdd);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#getSelectedComponent()
 	 */
 	public Caseless getSelectedComponent() {
 		return selectedComponent;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#setSelectedComponent(hr.fer.zemris.vhdllab.applets.schema2.misc.Caseless)
 	 */
 	public void setSelectedComponent(Caseless selectedComponent) {
 		Caseless temp = this.selectedComponent;
 		this.selectedComponent = selectedComponent;
-		support.firePropertyChange(propertyName, temp, selectedComponent);
+		support.firePropertyChange(PROPERTY_CHANGE_SELECTION, temp,
+				selectedComponent);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#getState()
 	 */
 	public ECanvasState getState() {
 		return state;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hr.fer.zemris.vhdllab.applets.schema2.gui.canvas.ILocalGuiController#setState(java.lang.Integer)
 	 */
 	public void setState(ECanvasState state) {
 		ECanvasState temp = this.state;
 		this.state = state;
-		support.firePropertyChange(propertyName, temp, state);
+		support.firePropertyChange(PROPERTY_CHANGE_COMPONENT_TO_ADD, temp,
+				state);
 	}
 }
