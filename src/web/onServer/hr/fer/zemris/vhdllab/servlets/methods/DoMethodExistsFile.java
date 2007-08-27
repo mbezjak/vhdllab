f89fa45dd21ce4c3b80e78e56344fb755ec38e50
Miro Bezjak <bezjak.miro@gmail.com>
2007-08-27 23:04:00 +0000

    dodana jos jedna metoda
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab/trunk/vhdllab@654 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ src/web/onServer/hr/fer/zemris/vhdllab/servlets/methods/DoMethodExistsFile.java
@@ -0,0 +1,45 @@
+package hr.fer.zemris.vhdllab.servlets.methods;
+
+import hr.fer.zemris.ajax.shared.MethodConstants;
+import hr.fer.zemris.vhdllab.communicaton.IMethod;
+import hr.fer.zemris.vhdllab.service.ServiceException;
+import hr.fer.zemris.vhdllab.service.VHDLLabManager;
+import hr.fer.zemris.vhdllab.servlets.AbstractRegisteredMethod;
+import hr.fer.zemris.vhdllab.servlets.ManagerProvider;
+
+import java.io.Serializable;
+
+/**
+ * This class represents a registered method for "exists file" request.
+ * 
+ * @author Miro Bezjak
+ * @see MethodConstants#MTD_EXISTS_FILE
+ */
+public class DoMethodExistsFile extends AbstractRegisteredMethod {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see hr.fer.zemris.vhdllab.servlets.RegisteredMethod#run(hr.fer.zemris.vhdllab.communicaton.IMethod,
+	 *      hr.fer.zemris.vhdllab.servlets.ManagerProvider)
+	 */
+	@Override
+	public void run(IMethod<Serializable> method, ManagerProvider provider) {
+		VHDLLabManager labman = getVHDLLabManager(provider);
+		Long fileId = method.getParameter(Long.class, PROP_ID);
+		if (fileId == null) {
+			return;
+		}
+		boolean exists;
+		try {
+			checkFileSecurity(method, labman, fileId);
+			exists = labman.existsFile(fileId);
+		} catch (ServiceException e) {
+			method.setStatus(SE_CAN_NOT_DETERMINE_EXISTANCE_OF_FILE,
+					"fileId=" + fileId);
+			return;
+		}
+		method.setResult(Boolean.valueOf(exists));
+	}
+
+}
