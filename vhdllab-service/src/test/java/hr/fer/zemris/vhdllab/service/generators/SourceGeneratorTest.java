1f23a845c2cf597f37f72595eced77ef062b88ec
Miro Bezjak <bezjak.miro@gmail.com>
2008-03-23 18:40:49 +0000

    added a test case for source generator and added svn:ignore properties in vhdllab-commons
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab2/trunk@883 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ vhdllab-service/src/test/java/hr/fer/zemris/vhdllab/service/generators/SourceGeneratorTest.java
@@ -0,0 +1,47 @@
+package hr.fer.zemris.vhdllab.service.generators;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+import hr.fer.zemris.vhdllab.api.FileTypes;
+import hr.fer.zemris.vhdllab.api.results.VHDLGenerationResult;
+import hr.fer.zemris.vhdllab.entities.File;
+import hr.fer.zemris.vhdllab.entities.Project;
+import hr.fer.zemris.vhdllab.service.VHDLGenerator;
+import hr.fer.zemris.vhdllab.test.FileContentProvider;
+import hr.fer.zemris.vhdllab.test.NameAndContent;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * A test case for {@link SourceGenerator}.
+ * 
+ * @author Miro Bezjak
+ */
+public class SourceGeneratorTest {
+
+    /**
+     * everything ok.
+     */
+    @Test
+    public void generateVHDL() throws Exception {
+        Project project = new Project("user.id", "project.name");
+        String type = FileTypes.VHDL_SOURCE;
+        List<NameAndContent> list = FileContentProvider.getContent(type);
+        assumeTrue(list.size() >= 1);
+        NameAndContent nc = list.get(0);
+        File f = new File(project, nc.getName(), type, nc.getContent());
+        VHDLGenerator gen = new SourceGenerator();
+        VHDLGenerationResult result = gen.generateVHDL(f);
+        assertTrue("result not successful.", result.isSuccessful());
+        assertEquals("status not equal.", Integer.valueOf(0), result
+                .getStatus());
+        assertEquals("messages is not empty.", Collections.emptyList(), result
+                .getMessages());
+        assertEquals("content not equal.", f.getContent(), result.getVHDL());
+    }
+
+}
