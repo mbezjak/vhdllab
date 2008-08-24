6c34e2d6a937fe8c0cc23d6a6766754b5f36948a
Miro Bezjak <bezjak.miro@gmail.com>
2008-08-24 20:53:10 +0000

    added old tests for client app
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab2/trunk@938 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ vhdllab-client/src/test/java/hr/fer/zemris/vhdllab/applets/main/conf/ComponentConfigurationParserNotJUnit.java
@@ -0,0 +1,26 @@
+package hr.fer.zemris.vhdllab.applets.main.conf;
+
+import hr.fer.zemris.vhdllab.applets.main.UniformAppletException;
+
+/**
+ * Tests {@link ComponentConfigurationParser} dialog, but just to see if its
+ * working. This is not an actual JUnit test.
+ * 
+ * @author Miro Bezjak
+ */
+public class ComponentConfigurationParserNotJUnit {
+
+	/**
+	 * Start of test. Not a JUnit test! Just testing to see if its working.
+	 * 
+	 * @param args
+	 *            no effect
+	 * @throws UniformAppletException
+	 *             there is nothing to do with exception
+	 */
+	public static void main(String[] args) throws UniformAppletException {
+		ComponentConfiguration conf = ComponentConfigurationParser
+				.getConfiguration();
+		System.out.println(conf);
+	}
+}
---
+++ vhdllab-client/src/test/java/hr/fer/zemris/vhdllab/client/dialogs/login/LoginDialogNotJUnit.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package hr.fer.zemris.vhdllab.client.dialogs.login;
+
+import java.awt.Frame;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Tests {@link LoginDialog}, but just GUI. This is not an actual JUnit test.
+ * 
+ * @author Miro Bezjak
+ */
+public class LoginDialogNotJUnit {
+
+	/**
+	 * Start of test. Not a JUnit test! Just testing GUI.
+	 * 
+	 * @param args
+	 *            no effect
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				initGUI();
+			}
+		});
+	}
+
+	private static void initGUI() {
+		Frame f = new Frame();
+		LoginDialog dialog = new LoginDialog(f);
+		dialog.setVisible(true);
+		System.out.println(dialog.getOption());
+		System.out.println(dialog.getCredentials());
+		f.dispose();
+	}
+
+}
