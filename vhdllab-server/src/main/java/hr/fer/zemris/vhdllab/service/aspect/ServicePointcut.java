5b7bdfcf8165cb1f00b877a047c5a435654b636c
Miro Bezjak <bezjak.miro@gmail.com>
2009-03-14 23:07:07 +0000

    fixed pointcut expressions
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab2/trunk@1085 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- vhdllab-server/src/main/java/hr/fer/zemris/vhdllab/service/aspect/ServicePointcut.java
+++ vhdllab-server/src/main/java/hr/fer/zemris/vhdllab/service/aspect/ServicePointcut.java
@@ -4,21 +4,12 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class ServicePointcut {
 
-    @Pointcut("remoteServices() || execution(* hr.fer.zemris.vhdllab.service.filetype.DependencyExtractor.*(..))")
-            public void services() {
-    }
-
     @Pointcut("execution(* hr.fer.zemris.vhdllab.service.FileService.*(..)) || "
             + "execution(* hr.fer.zemris.vhdllab.service.ProjectService.*(..)) ||"
-            + "execution(* hr.fer.zemris.vhdllab.service.LibraryFileService.*(..)) ||"
-            + "execution(* hr.fer.zemris.vhdllab.service.UserFileService.*(..)) ||"
-            + "execution(* hr.fer.zemris.vhdllab.service.WorkspaceService.*(..)) ||"
-            + "execution(* hr.fer.zemris.vhdllab.service.Compiler.*(..)) ||"
+            + "execution(* hr.fer.zemris.vhdllab.service.ClientLogService.*(..)) ||"
             + "execution(* hr.fer.zemris.vhdllab.service.Simulator.*(..)) ||"
-            + "execution(* hr.fer.zemris.vhdllab.service.HierarchyExtractor.*(..)) ||"
-            + "execution(* hr.fer.zemris.vhdllab.service.filetype.CircuitInterfaceExtractor.*(..)) ||"
-            + "execution(* hr.fer.zemris.vhdllab.service.filetype.VhdlGenerator.*(..))")
-    public void remoteServices() {
+            + "execution(* hr.fer.zemris.vhdllab.service.MetadataExtractionService.*(..))")
+    public void services() {
     }
 
 }
