b3d53a5f7acb879805ce216c314cbc4b05edf8c4
Miro Bezjak <bezjak.miro@gmail.com>
2009-04-03 17:24:59 +0000

    development menu will now be visible only in development environment!
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab2/trunk@1172 cbae1e92-611c-0410-ab7b-b19ac7dee622

---
+++ vhdllab-client/src/main/java/hr/fer/zemris/vhdllab/platform/support/SelectiveCommandGroupFactoryBean.java
@@ -0,0 +1,32 @@
+package hr.fer.zemris.vhdllab.platform.support;
+
+import hr.fer.zemris.vhdllab.platform.context.ApplicationContextHolder;
+
+import org.springframework.richclient.command.CommandGroup;
+import org.springframework.richclient.command.CommandGroupFactoryBean;
+
+public class SelectiveCommandGroupFactoryBean extends CommandGroupFactoryBean {
+
+    @Override
+    protected void initCommandGroupMembers(CommandGroup group) {
+        Object[] members = getMembers();
+        for (int i = 0; i < members.length; i++) {
+            Object o = members[i];
+            if (isDevelopmentMenu(o)
+                    && !ApplicationContextHolder.getContext().isDevelopment()) {
+                /*
+                 * Development menu is only initialized in a development
+                 * environment!
+                 */
+                getMembers()[i] = null;
+            }
+        }
+        super.initCommandGroupMembers(group);
+    }
+
+    private boolean isDevelopmentMenu(Object o) {
+        return o instanceof CommandGroup
+                && ((CommandGroup) o).getId().equals("developmentMenu");
+    }
+
+}
