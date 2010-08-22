7f48d9cb50c3dff9b09d84eacbce1a7db129b388
Miro Bezjak <bezjak.miro@gmail.com>
2010-08-22 16:26:19 +0000

    izbrisan interni ghdl koji se koristio i ponovno uspostavljen dependency na vanjsko instalirani ghdl
    git-svn-id: http://morgoth.zemris.fer.hr/svn/vhdllab/vhdllab2/trunk@1225 cbae1e92-611c-0410-ab7b-b19ac7dee622

--- vhdllab-server/src/main/java/hr/fer/zemris/vhdllab/service/impl/GhdlSimulator.java
+++ vhdllab-server/src/main/java/hr/fer/zemris/vhdllab/service/impl/GhdlSimulator.java
@@ -17,24 +17,21 @@ import hr.fer.zemris.vhdllab.service.hierarchy.HierarchyNode;
 import hr.fer.zemris.vhdllab.service.result.CompilationMessage;
 import hr.fer.zemris.vhdllab.service.result.Result;
 import hr.fer.zemris.vhdllab.service.util.SecurityUtils;
-import hr.fer.zemris.vhdllab.service.util.WebUtils;
 import hr.fer.zemris.vhdllab.util.IOUtil;
 import hr.fer.zemris.vhdllab.util.StringUtil;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-
-import javax.annotation.PostConstruct;
-import javax.servlet.ServletContext;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
@@ -44,12 +41,11 @@ import org.apache.commons.exec.ExecuteWatchdog;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.SystemUtils;
 import org.apache.commons.lang.UnhandledException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.ServletContextAware;
 
 /**
  * A wrapper for GHDL (http://ghdl.free.fr/).
@@ -59,8 +55,7 @@ import org.springframework.web.context.ServletContextAware;
  * @version 1.0
  * @since vhdllab2
  */
-public class GhdlSimulator extends ServiceSupport implements Simulator,
-        ServletContextAware {
+public class GhdlSimulator extends ServiceSupport implements Simulator {
 
     /**
      * Logger for this class
@@ -72,148 +67,38 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
     private static final int ACQUIRE_TIMEOUT = 1;
     private static final TimeUnit ACQUIRE_TIME_UNIT = TimeUnit.SECONDS;
 
-    private static final String GHDL_BASE_DIR = "/WEB-INF/ghdl/";
-    private static final String DEFAULT_TMP_ROOT_DIR = GHDL_BASE_DIR + "tmp";
+    private static final String PROP_COMPILATION_CMD = "compilation.cmd";
+    private static final String PROP_SIMULATION_CMD = "simulation.cmd";
 
-    /*
-     * Linux properties:
-     */
-    private static final String LINUX_BASE_DIR = GHDL_BASE_DIR + "linux/";
-    private static final String LINUX_EXECUTABLE = LINUX_BASE_DIR + "bin/ghdl";
-    private static final String LINUX_GHDL1_PATH = LINUX_BASE_DIR
-            + "libexec/gcc/i686-pc-linux-gnu/4.2.4/ghdl1";
-    private static final String LINUX_PREFIX_PATH = LINUX_BASE_DIR
-            + "lib/gcc/i686-pc-linux-gnu/4.2.4/vhdl/lib/";
-    private static final String LINUX_COMMON_PARAMETERS = "-Wc,-m32 -Wa,--gstabs -Wa,--32 -Wl,-m32 --GHDL1={0} --PREFIX={1}";
-    private static final String LINUX_COMPILATION_PARAMETERS = "-a "
-            + LINUX_COMMON_PARAMETERS;
-    private static final String LINUX_ELABORATION_PARAMETERS = "-e "
-            + LINUX_COMMON_PARAMETERS + " -o run_simulation";
-
-    /*
-     * Windows properties:
-     */
-    private static final String WINDOWS_EXECUTABLE = GHDL_BASE_DIR
-            + "windows/bin/ghdl.exe";
-    private static final String WINDOWS_COMPILATION_PARAMETERS = "-a";
-
-    private ServletContext servletContext;
-
-    private java.io.File tmpRootDir;
-    private java.io.File executable;
-    private String[] compilationParameters;
-    private String[] elaborationParameters;
+    private final Semaphore semaphore = new Semaphore(MAX_SIMULTANEOUS_PROCESSES);
 
     private boolean leaveSimulationResults = true;
 
-    private final Semaphore semaphore = new Semaphore(
-            MAX_SIMULTANEOUS_PROCESSES);
+    private Properties properties;
 
     @Autowired
     private WorkspaceService workspaceService;
 
-    @PostConstruct
-    public void extractLinuxGhdl() throws IOException {
-        if (SystemUtils.IS_OS_UNIX) {
-            java.io.File workingDir = asFile(GHDL_BASE_DIR);
-            String tarCommand = "tar xf "
-                    + asFile(GHDL_BASE_DIR + "ghdl-linux.tar").getPath();
-            LOG.debug("Executing: " + tarCommand + " in directory: "
-                    + workingDir.getPath());
-            Runtime.getRuntime().exec(tarCommand, null, workingDir);
-
-            String executablePath = getExecutable().getPath();
-            String ghdl1 = asFile(LINUX_GHDL1_PATH).getPath();
-            String chmodCommand = "chmod u+x " + executablePath + " " + ghdl1;
-            LOG.debug("Executing: " + chmodCommand);
-            Runtime.getRuntime().exec(chmodCommand);
+    private Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream("ghdl.properties");
+            try {
+                properties.load(is);
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+            IOUtils.closeQuietly(is);
         }
+
+        return properties;
     }
 
     @Override
-    public void setServletContext(ServletContext servletContext) {
-        this.servletContext = servletContext;
-    }
-
-    public java.io.File getTmpRootDir() {
-        if (tmpRootDir == null) {
-            tmpRootDir = asFile(DEFAULT_TMP_ROOT_DIR);
-        }
-        return tmpRootDir;
-    }
-
-    public void setTmpRootDir(java.io.File tmpRootDir) {
-        this.tmpRootDir = tmpRootDir;
-    }
-
-    public java.io.File getExecutable() {
-        if (executable == null) {
-            executable = asFile(getBasedOnOS(LINUX_EXECUTABLE,
-                    WINDOWS_EXECUTABLE));
-        }
-        return executable;
-    }
-
-    public void setExecutable(java.io.File executable) {
-        this.executable = executable;
-    }
-
-    public String[] getCompilationParameters() {
-        if (compilationParameters == null) {
-            compilationParameters = getBasedOnOS(
-                    createLinuxParameters(LINUX_COMPILATION_PARAMETERS),
-                    WINDOWS_COMPILATION_PARAMETERS).split(" ");
-        }
-        return compilationParameters;
-    }
-
-    public void setCompilationParameters(String[] compilationParameters) {
-        this.compilationParameters = compilationParameters;
-    }
-
-    public String[] getElaborationParameters() {
-        if (elaborationParameters == null) {
-            elaborationParameters = createLinuxParameters(
-                    LINUX_ELABORATION_PARAMETERS).split(" ");
-        }
-        return elaborationParameters;
-    }
-
-    public void setElaborationParameters(String[] elaborationParameters) {
-        this.elaborationParameters = elaborationParameters;
-    }
-
-    private java.io.File asFile(String path) {
-        return WebUtils.getLocation(servletContext, path);
-    }
-
-    private String getBasedOnOS(String linuxProperty, String windowsProperty) {
-        if (SystemUtils.IS_OS_UNIX) {
-            return linuxProperty;
-        } else if (SystemUtils.IS_OS_WINDOWS) {
-            return windowsProperty;
-        } else {
-            throw new IllegalStateException("GHDL is unsupported on: "
-                    + SystemUtils.OS_NAME);
-        }
-    }
-
-    private String createLinuxParameters(String parameters) {
-        String ghdl1 = asFile(LINUX_GHDL1_PATH).getPath();
-        /*
-         * prefix must end with / character otherwise process wont execute!
-         */
-        String prefix = asFile(LINUX_PREFIX_PATH).getPath() + "/";
-        return MessageFormat.format(parameters, ghdl1, prefix);
-    }
-
-    @Override
-    public List<CompilationMessage> compile(Integer fileId)
-            throws CompilationException {
+    public List<CompilationMessage> compile(Integer fileId) throws CompilationException {
         try {
             if (!semaphore.tryAcquire(ACQUIRE_TIMEOUT, ACQUIRE_TIME_UNIT)) {
-                throw new NoAvailableProcessException(
-                        MAX_SIMULTANEOUS_PROCESSES);
+                throw new NoAvailableProcessException(MAX_SIMULTANEOUS_PROCESSES);
             }
         } catch (InterruptedException e) {
             throw new NoAvailableProcessException(e);
@@ -227,8 +112,7 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
     public Result simulate(Integer fileId) throws SimulationException {
         try {
             if (!semaphore.tryAcquire(ACQUIRE_TIMEOUT, ACQUIRE_TIME_UNIT)) {
-                throw new NoAvailableProcessException(
-                        MAX_SIMULTANEOUS_PROCESSES);
+                throw new NoAvailableProcessException(MAX_SIMULTANEOUS_PROCESSES);
             }
         } catch (InterruptedException e) {
             throw new NoAvailableProcessException(e);
@@ -257,13 +141,9 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
         String waveform;
         try {
             doCompile(context);
-            if (SystemUtils.IS_OS_UNIX) {
-                prepairElaborationCommandLine(context);
-                executeProcess(context.commandLine, context.tempDirectory);
-            }
+
             prepairSimulationCommandLine(context);
-            context.result = executeProcess(context.commandLine,
-                    context.tempDirectory);
+            context.result = executeProcess(context.commandLine, context.tempDirectory);
             waveform = extractWaveform(context);
         } catch (IOException e) {
             throw new CompilationException(e);
@@ -285,19 +165,13 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
 
     private void doCompile(SimulationContext context) throws IOException {
         try {
-            context.tempDirectory = createTempDirectory(context.targetFile
-                    .getId());
-
+            context.tempDirectory = createTempDirectory(context.targetFile.getId());
             context.dependencies = orderFileNames(context.targetFile);
 
-            copyFiles(context.dependencies, context.targetFile.getProject()
-                    .getId(), context.tempDirectory);
+            copyFiles(context.dependencies, context.targetFile.getProject().getId(), context.tempDirectory);
 
-            context.commandLine = prepairCompilerCommandLine(
-                    getCompilationParameters(), context.dependencies);
-
-            context.result = executeProcess(context.commandLine,
-                    context.tempDirectory);
+            context.commandLine = prepairCompilationCommandLine(context.dependencies);
+            context.result = executeProcess(context.commandLine, context.tempDirectory);
         } finally {
             if (context.compileOnly) {
                 FileUtils.deleteQuietly(context.tempDirectory);
@@ -306,8 +180,7 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
     }
 
     private List<String> orderFileNames(File file) {
-        Hierarchy hierarchy = workspaceService.extractHierarchy(file
-                .getProject().getId());
+        Hierarchy hierarchy = workspaceService.extractHierarchy(file.getProject().getId());
         List<File> ordered = new ArrayList<File>();
         orderFileNames(ordered, hierarchy, hierarchy.getNode(file));
         List<String> names = new ArrayList<String>(ordered.size());
@@ -317,8 +190,7 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
         return names;
     }
 
-    private void orderFileNames(List<File> ordered, Hierarchy hierarchy,
-            HierarchyNode node) {
+    private void orderFileNames(List<File> ordered, Hierarchy hierarchy, HierarchyNode node) {
         Set<HierarchyNode> dependencies = hierarchy.getDependenciesFor(node);
         for (HierarchyNode n : dependencies) {
             orderFileNames(ordered, hierarchy, n);
@@ -329,66 +201,49 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
     }
 
     private java.io.File createTempDirectory(Integer fileId) throws IOException {
-        String suffix = "_by_user-" + SecurityUtils.getUser() + "__file_id-"
-                + fileId;
-        java.io.File tempFile = java.io.File.createTempFile("ghd", suffix,
-                getTmpRootDir());
-        java.io.File tempDir = new java.io.File(getTmpRootDir(), "DIR"
-                + tempFile.getName());
+        String suffix = "_by_user-" + SecurityUtils.getUser() + "__file_id-" + fileId;
+        java.io.File tempFile = java.io.File.createTempFile("ghd", suffix, null);
+        java.io.File tempDir = new java.io.File(tempFile.getParentFile(), "DIR" + tempFile.getName());
         FileUtils.forceMkdir(tempDir);
         FileUtils.deleteQuietly(tempFile);
         LOG.debug("Created temp directory: " + tempDir.getPath());
         return tempDir;
     }
 
-    private void copyFiles(List<String> dependencies, Integer projectId,
-            java.io.File tempDirectory) throws IOException {
+    private void copyFiles(List<String> dependencies, Integer projectId, java.io.File tempDirectory) throws IOException {
         for (String dep : dependencies) {
             File depFile = findProjectOrPredefinedFile(projectId, dep);
             String data = depFile.getData();
             if (!depFile.getType().equals(FileType.PREDEFINED)) {
                 data = metadataExtractor.generateVhdl(depFile).getData();
             }
-            java.io.File fileOnDisk = new java.io.File(tempDirectory, dep
-                    + ".vhdl");
-            FileUtils.writeStringToFile(fileOnDisk, data,
-                    IOUtil.DEFAULT_ENCODING);
+            java.io.File fileOnDisk = new java.io.File(tempDirectory, dep + ".vhdl");
+            FileUtils.writeStringToFile(fileOnDisk, data, IOUtil.DEFAULT_ENCODING);
         }
     }
 
-    private CommandLine createCommandLine(java.io.File executableCommand) {
-        return new CommandLine(executableCommand);
+    private CommandLine createCommandLine(String cmd) {
+        return CommandLine.parse(cmd);
     }
 
-    private CommandLine prepairCompilerCommandLine(String[] arguments,
-            List<String> dependencies) {
-        CommandLine cl = createCommandLine(getExecutable());
-        cl.addArguments(arguments);
-        for (String name : dependencies) {
-            cl.addArgument(name + ".vhdl");
+    private CommandLine prepairCompilationCommandLine(List<String> dependencies) {
+        String cmd = getProperties().getProperty(PROP_COMPILATION_CMD);
+
+        String[] files = new String[dependencies.size()];
+        for (int i = 0; i < dependencies.size(); i++) {
+            files[i] = dependencies.get(i) + ".vhdl";
         }
-        return cl;
-    }
+        cmd = cmd.replace("{files}", StringUtils.join(files, ' '));
 
-    private void prepairElaborationCommandLine(SimulationContext context) {
-        CommandLine cl = createCommandLine(getExecutable());
-        cl.addArguments(getElaborationParameters());
-        cl.addArgument(context.targetFile.getName());
-        context.commandLine = cl;
+        return createCommandLine(cmd);
     }
 
     private void prepairSimulationCommandLine(SimulationContext context) {
-        CommandLine cl;
-        if (SystemUtils.IS_OS_UNIX) {
-            cl = createCommandLine(new java.io.File(context.tempDirectory,
-                    "run_simulation"));
-        } else { // else windows
-            cl = createCommandLine(getExecutable());
-            cl.addArgument("-r");
-            cl.addArgument(context.targetFile.getName());
-        }
+        String cmd = getProperties().getProperty(PROP_SIMULATION_CMD);
+        cmd = cmd.replace("{files}", context.targetFile.getName());
+
+        CommandLine cl = createCommandLine(cmd);
         cl.addArgument("--vcd=simout.vcd");
-        cl.addArgument("--stop-delta=1000");
 
         Testbench tb = null;
         try {
@@ -400,18 +255,16 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
         if (simulationLength <= 0) {
             simulationLength = 100;
         } else {
-            simulationLength = simulationLength
-                    / TimeScale.getMultiplier(tb.getTimeScale());
+            simulationLength = simulationLength / TimeScale.getMultiplier(tb.getTimeScale());
             simulationLength = (long) (simulationLength * 1.1);
         }
-        cl.addArgument("--stop-time=" + simulationLength
-                + tb.getTimeScale().toString().toLowerCase());
+        cl.addArgument("--stop-time=" + simulationLength + tb.getTimeScale().toString().toLowerCase());
 
         context.commandLine = cl;
     }
 
-    private List<String> executeProcess(CommandLine cl,
-            java.io.File tempDirectory) throws ExecuteException, IOException {
+    private List<String> executeProcess(CommandLine cl, java.io.File tempDirectory) throws ExecuteException,
+            IOException {
         if (LOG.isDebugEnabled()) {
             LOG.debug("Executing process: " + cl.toString());
         }
@@ -454,16 +307,13 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
         return Arrays.asList(StringUtil.splitToNewLines(output));
     }
 
-    private List<CompilationMessage> listToCompilationMessages(
-            List<String> errors) {
-        List<CompilationMessage> list = new ArrayList<CompilationMessage>(
-                errors.size());
+    private List<CompilationMessage> listToCompilationMessages(List<String> errors) {
+        List<CompilationMessage> list = new ArrayList<CompilationMessage>(errors.size());
         for (String e : errors) {
             String[] line = parseGHDLErrorMessage(e);
             if (line.length == 4) {
                 list
-                        .add(new CompilationMessage(line[0], Integer
-                                .parseInt(line[1]), Integer.parseInt(line[2]),
+                        .add(new CompilationMessage(line[0], Integer.parseInt(line[1]), Integer.parseInt(line[2]),
                                 line[3]));
             } else {
                 list.add(new CompilationMessage(line[1]));
@@ -484,8 +334,7 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
             } else {
                 return new String[] {
                         res[0],
-                        (res[1] == null ? "" : res[1])
-                                + (res[2] == null ? "" : ":" + res[2])
+                        (res[1] == null ? "" : res[1]) + (res[2] == null ? "" : ":" + res[2])
                                 + (res[3] == null ? "" : ":" + res[3]) };
             }
         }
@@ -497,16 +346,15 @@ public class GhdlSimulator extends ServiceSupport implements Simulator,
         return res;
     }
 
-    private String extractWaveform(SimulationContext context)
-            throws IOException {
-        String vcd = FileUtils.readFileToString(new java.io.File(
-                context.tempDirectory, "simout.vcd"), IOUtil.DEFAULT_ENCODING);
+    private String extractWaveform(SimulationContext context) throws IOException {
+        String vcd = FileUtils.readFileToString(new java.io.File(context.tempDirectory, "simout.vcd"),
+                IOUtil.DEFAULT_ENCODING);
         VcdParser parser = new VcdParser(StringUtil.splitToNewLines(vcd));
         parser.parse();
         return parser.getResultInString();
     }
 
-    private class SimulationContext {
+    private static class SimulationContext {
         /*
          * Since this is a private class used by simulator exclusively there is
          * no need for getters and setters.
