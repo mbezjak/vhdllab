7eb362e25c3c56c59197a34a6679a7acfec0e73f
Miro Bezjak <bezjak.miro@gmail.com>
2016-01-11 11:31:08 +0100

    poly-devel customizations are no longer needed.
    Default gradle behaviour is the correct one.

--- poly-devel/update-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-
-declare -r version="$1"
-declare -r oldxml='<application.version>.\+</application.version>'
-declare -r newxml="<application.version>$version</application.version>"
-
-sed -i "s|$oldxml|$newxml|" pom.xml
-properties-set build.gradle version "'$version'"
--- poly-devel/version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-
-cat pom.xml | xpath-query '//project/properties/application.version/text()'
